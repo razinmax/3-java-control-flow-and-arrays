f7f1fd784cb79caa1f663ef6352559e7399b8b4b
Max Razin <razinmax02@gmail.com>
2024-10-24 11:42:19 +0500

    Task09 complete

--- task09/src/com.example.task09/Task09Main.java
+++ task09/src/com.example.task09/Task09Main.java
@@ -4,18 +4,22 @@ public class Task09Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         int[] arr = new int[3];
         arr[0] = 2;
         arr[1] = -1;
         arr[2] = 3;
         System.out.println(min(arr));
-         */
     }
 
     static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        if (arr.length == 0){
+            return 0;
+        }
+        int min = arr[0];
+        for (int num : arr){
+            if (num < min)
+                min = num;
+        }
+        return min;
     }
-
 }
