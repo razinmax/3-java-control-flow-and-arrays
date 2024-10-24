c97197009931972661d2e03457d6e47891808fb8
Max Razin <razinmax02@gmail.com>
2024-10-24 10:36:59 +0500

    Task02 complete

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -4,14 +4,11 @@ public class Task01Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-
         boolean b = canGetDriverLicense(100);
         System.out.println(b);
-
     }
 
     static boolean canGetDriverLicense(int age) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         return age >= 18;
     }
 }
--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -3,13 +3,25 @@ package com.example.task02;
 public class Task02Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+        // например вот так
         System.out.println(getSeason(12));
-         */
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        // Проверяем, что номер месяца находится в допустимом диапазоне
+        if (monthNumber < 1 || monthNumber > 12) {
+            return "Некорректный номер месяца"; // Возвращаем сообщение об ошибке
+        }
+
+        // Определяем время года по номеру месяца
+        if (monthNumber == 12 || monthNumber == 1 || monthNumber == 2) {
+            return "зима";   // Декабрь, Январь, Февраль
+        } else if (monthNumber <= 5) {
+            return "весна";  // Март, Апрель, Май
+        } else if (monthNumber <= 8) {
+            return "лето";   // Июнь, Июль, Август
+        } else {
+            return "осень";  // Сентябрь, Октябрь, Ноябрь
+        }
     }
 }
