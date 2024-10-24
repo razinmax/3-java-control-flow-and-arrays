f2fe3a820b5a23a8994554d5cd019099c1f637da
Max Razin <razinmax02@gmail.com>
2024-10-24 12:44:28 +0500

    Task13 complete

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -4,16 +4,31 @@ public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+        int[] result = removeMoreThen1000(arr);
+        System.out.println(java.util.Arrays.toString(result));
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        if (arr == null || arr.length == 0) {
+            return arr;
+        }
+        //считаем количество элементов, которые меньше или равны 1000
+        int count = 0;
+        for (int num : arr){
+            if (num <= 1000){
+                count++;
+            }
+        }
+        //Создаем новый массив для записи элементов меньше или равных 1000
+        int[] result = new int[count];
+        int index = 0;
+        for (int num : arr){
+            if (num <= 1000){
+                result[index++] = num;
+            }
+        }
+        return result;
     }
 
 }
